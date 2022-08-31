c31287f592270d521511ea9821a2098f0e2ef526
김주엽 <kjye.ops@gmail.com>
2022-08-31 19:15:53 +0900

    Remove overlapped file

--- Programmers/숫자__문자열과_영단어.py
+++
@@ -1,7 +0,0 @@
-def solution(s):
-    num_dict = {"zero": 0,"one": 1,"two": 2,"three": 3,"four": 4,"five": 5,"six": 6, "seven": 7, "eight": 8, "nine": 9}
-    answer = s
-    for i in num_dict:
-        answer = answer.replace(i, str(num_dict[i]))
-
-    return int(answer)
