e251fdcd629fcf5161305ab2067ae32932398a99
juye-ops <kjye.ops@gmail.com>
2022-09-03 18:36:33 +0900

    Revised 124 나라의 숫자, 다리를 지나는 트럭 to be more faster.

--- Programmers/124_나라의_숫자.py
+++ Programmers/124_나라의_숫자.py
@@ -1,25 +1,13 @@
 def solution(n):
-    a = [1,2,4]
-    newa = []
-    cnt = 0
-    squarelist = []
-    mm = 1
-    while 3**mm < 500000000:
-        squarelist.append(3**mm)
-        mm+=1
-
-    for i in squarelist:
-        if i<n:
-            n-=i
-            cnt += 1
-
-    n-=1
-    for i in range(cnt+1):
-        newa.append(a[(n%3)])
-        n//=3
-    realsum = 0
-    for i in range(len(newa)):
-        realsum+= newa[i] * (10**i)
-
-
-    return str(realsum)
+    answer = ''
+    lst = []
+    dct = {0: "1", 1: "2", 2: "4"}
+    while True:
+        n-=1
+        lst.append(n % 3)
+        n = n//3
+        if n <= 0:
+            break
+    lst.reverse()
+    
+    return "".join(list(map(lambda x: dct[x], lst)))
--- Programmers/다리를_지나는_트럭.py
+++ Programmers/다리를_지나는_트럭.py
@@ -1,14 +1,19 @@
 def solution(bridge_length, weight, truck_weights):
     bridge = [0] * bridge_length
     time = 1
+    sumval = 0
     while truck_weights:
         value = 0
 
-        bridge.pop(0)
-        if sum(bridge) + truck_weights[0] <= weight:
+        a = bridge.pop(0)
+        sumval-=a
+        if sumval + truck_weights[0] <= weight:
             value = truck_weights.pop(0)
         bridge.append(value)
+        sumval+=value
 
         time+=1
     time+=bridge_length - 1
     return time
+
+solution(2, 10, [7, 4, 5, 6])
