105ade4c772e12a2b7d937176a799e10d66d6dc7
juye-ops <kjye.ops@gmail.com>
2023-03-22 14:52:24 +0900

    최대 힙(1927) on 2023-03-22

---
+++ Baekjoon/11279.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+from heapq import heappop, heappush
+
+def solution(N, queries):
+    heap = []
+    for i in queries:
+        if i:
+            heappush(heap, -i)
+        else:
+            target = 0
+            if heap:
+                target = -heappop(heap)
+            print(target)
+        
+    
+N = int(input())
+queries = [int(input()) for _ in range(N)]
+
+solution(N, queries)
