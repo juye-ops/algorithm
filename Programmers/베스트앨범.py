d83ba859988b24d12f119f8eedaff008f44053fa
juye-ops <kjye.ops@gmail.com>
2023-04-11 17:08:15 +0900

    Refactor: 베스트앨범 on 2023-04-11

--- Programmers/베스트앨범.py
+++ Programmers/베스트앨범.py
@@ -1,10 +1,16 @@
-from collections import defaultdict
-
 def solution(genres, plays):
-    a = defaultdict(lambda: [])
-    for i, x in enumerate(zip(genres, plays)):
-        a[x[0]].append((i, x[1]))
-    a = sorted(a.values(), key = lambda x: sum(map(lambda y: y[1], x)), reverse=True)
-    a = list(map(lambda x: sorted(x, key = sum, reverse=True)[:2], a))
-    a = [x if len(x) != 2 or x[0][1] != x[1][1] else [x[1], x[0]] for x in a]
-    return [x[0] for x in sum(a, [])]
+    music = {}
+    answer = []
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        if music.get(g) is None:
+            music[g] = []
+        music[g].append((p, i))
+
+    for i in music:
+        music[i].sort(key = lambda x: -x[0])
+    music = sorted(list(music.values()), key = lambda x: sum(list(zip(*x))[0]))
+    
+    for x in reversed(music):
+        for j in x[:2]:
+            answer.append(j[1])
+    return answer
